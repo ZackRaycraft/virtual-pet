1bba57c4d19fb7181b6faa18542a4a29acc3ec9a
Zack Raycraft <raycraftzack@aol.com>
2017-01-24 08:24:14 -0500

    Finished project.

---
+++ VirtualPet/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Greetings user! Meet your virtual pet Terry the Squirrel!");
+            Console.WriteLine();
+
+            Squirrel terry = new Squirrel(10, 10, 10);
+
+            //This is where the program will re-start after the loop has ran
+            start:
+
+            //The program will write out Terry's hunger, thirst, and bathroom levels
+            terry.PrintPetInfo();
+            Console.WriteLine();
+
+            //The program will write out the different options to interact with Terry
+            terry.PrintPetOptions();
+            Console.WriteLine();
+            int userInput = int.Parse(Console.ReadLine());
+
+            
+            while(userInput == userInput)
+            {
+                if (userInput == 1)
+                {
+                    terry.FeedTerry();
+                    Console.WriteLine();
+                    goto start;
+                }
+
+                else if (userInput == 2)
+                {
+                    terry.WaterTerry();
+                    Console.WriteLine();
+                    goto start;
+                }
+
+                else if (userInput == 3)
+                {
+                    terry.TakeTerryPotty();
+                    Console.WriteLine();
+                    goto start;
+                }
+
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Please enter a number 1-3");
+                    Console.WriteLine();
+                    goto start;
+                }
+            }
+
+
+        }
+    }
+}
---
+++ VirtualPet/Squirrel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Squirrel
+    {
+        private int hunger;
+        private int thirst;
+        private int waste;
+
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Thirst
+        {
+            get { return this.thirst; }
+            set { this.thirst = value; }
+        }
+
+        public int Waste
+        {
+            get { return this.waste; }
+            set { this.waste = value; }
+        }
+
+        public Squirrel(int hunger, int thirst, int waste)
+        {
+            this.hunger = hunger;
+            this.thirst = thirst;
+            this.waste = waste;
+        }
+
+        public void PrintPetInfo()
+        {
+            Console.WriteLine("Terry the Squirrel");
+            Console.WriteLine("Hunger: " + hunger);
+            Console.WriteLine("Thirst: " + thirst);
+            Console.WriteLine("Waste: " + waste);
+        }
+
+        public void PrintPetOptions()
+        {
+            Console.WriteLine("What do you wish to do? Please select a number 1-3!");
+            Console.WriteLine("1. Feed Terry");
+            Console.WriteLine("2. Give Terry waster");
+            Console.WriteLine("3. Take Terry outside to go potty");
+        }
+
+        public int FeedTerry()
+        {
+            hunger--;
+            thirst++;
+            return hunger;
+        }
+
+        public int WaterTerry()
+        {
+            thirst--;
+            waste++;
+            return thirst;
+        }
+
+        public int TakeTerryPotty()
+        {
+            waste--;
+            return waste;
+        }
+    }
+
+   
+}
